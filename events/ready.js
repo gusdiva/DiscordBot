846089cdef554758b1fb5c49a7fbdddb9287f40c
I Putu Bagus Diva Mahendra <wixxbali@gmail.com>
2021-03-02 18:44:04 +0800

    PRESENCE

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,27 @@
+const client = require("../index");
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("IZ*ONE", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  
-  });
+
+  const arrayOfStatus = [
+    `IZ*ONE`,
+    `ONE-REELER/ACT IV`,
+    `ONEIRIC DIARY`,
+    `BLOOM*IZ`,
+    `HEART*IZ`,
+    `COLOR*IZ`,
+    `${client.guilds.cache.size} servers`,
+    //`${client.channels.cache.size} channels`,
+    `${client.users.cache.size} users`,
+    //`${client.user.tag} discord bot!`,
+    `${client.config.prefix}help`
+  ];
+  let index = 0;
+  setInterval(() => {
+    if (index === arrayOfStatus.length) index =0;
+    const status = arrayOfStatus[index];
+    console.log(status);
+    client.user.setActivity(status,{type: "LISTENING"});
+    index++;
+  }, 10000)
+
 };
